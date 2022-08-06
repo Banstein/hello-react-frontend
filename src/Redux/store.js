56ff3bd5ba75f1793ce50ff71740ea1fb94d377e
Islam Bahnas <islambahnas1@gmail.com>
2022-08-06 21:16:47 +0200

    reset

--- src/Redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import greetings from './greetings/greetings';
-
-const rootReducer = combineReducers({ greetings });
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
